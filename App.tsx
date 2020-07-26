f835ab7bc00e29ca9ba105b3fcbfdae37d739e0f
Elcoss <jp14700@gmail.com>
2020-07-26 13:32:33 -0300

    add roboto and ubuntu fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import Routes from './src/routes'
 import { StatusBar } from 'react-native'
+import { AppLoading } from 'expo'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [loaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  })
+
+  if (!loaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
